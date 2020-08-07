22542d386bf29616b23975f9548437e504903287
Jay Barr <jayrbarr@gmail.com>
2020-08-07 11:09:45 -0400

    Completes 19 Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,106 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(localMediaStream => {
+    video.srcObject = localMediaStream;
+    video.play();
+  })
+  .catch(err => {
+    console.error('OH NO!!!', err);
+  });
+}
+
+function redEffect(pixels) {
+  pixels.data.forEach((_, idx, pixels) => {
+    switch (idx%4) {
+      case 0:
+        pixels[idx] += 100;
+        break;
+      case 1:
+        pixels[idx] -= 50;
+        break;
+      case 2:
+        pixels[idx] *= 0.5;
+        break;
+      default:
+        break;
+    }
+  });
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  pixels.data.forEach((_, idx, pixels) => {
+    switch (idx%4) {
+      case 0:
+        pixels[idx - 59] = pixels[idx];
+        break;
+      case 1:
+        pixels[idx + 100] = pixels[idx];
+        break;
+      case 2:
+        pixels[idx - 150] = pixels[idx];
+        break;
+      default:
+        break;
+    }
+  });
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value;
+  });
+  for (let idx = 0; idx < pixels.data.length; idx = idx +4) {
+      const red = pixels.data[idx];
+      const green = pixels.data[idx+1];
+      const blue = pixels.data[idx+2];
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+          pixels.data[idx+3] = 0;
+        }
+    }
+  return pixels;
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
+    // ctx.globalAlpha = 0.8;
+  }, 16);
+  
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
